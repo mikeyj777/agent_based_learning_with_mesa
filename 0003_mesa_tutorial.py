1239ef72eb13cc4bbe9ddca1749d30464bcb4f94
Michael James <mikeyj777@gmail.com>
2024-09-07 22:07:23 -0400

    setup

--- 0003_mesa_tutorial.py
+++ 0003_mesa_tutorial.py
@@ -1,4 +1,5 @@
 import mesa
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -28,41 +29,79 @@ class Spice(mesa.Agent):
 class Trader(mesa.Agent):
     # trader has a metabolism (processes sugar and spice per turn)
     # harvest sugar and spice
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, pos, moore=False, sugar=0, spice=0, metabolism_sugar=0, metabolism_spice=0, vision=0):
         super().__init__(unique_id, model)
         
+        self.pos = pos
+        self.moore = moore
+        self.sugar = sugar
+        self.spice = spice
+        self.metabolism_sugar = metabolism_sugar
+        self.metabolism_spice = metabolism_spice
+        self.vision = vision
+
 class SugarscapeG1mt(mesa.Model):
     '''
     model class to manage sugarscape with traders (g1mt)
     from Growing Artificial Societies - Axtell and Epstien
     '''
-    def __init__(self, width = 50, height = 50):
+    def __init__(self, width = 50, height = 50, initial_population = 200, endowment_min = 25, endowment_max = 50, metabolism_min = 1, metabolism_max = 50, vision_min=1, vision_max=50):
+
         super().__init__()
         self.width = width
         self.height = height
+        self.initial_population = initial_population
+        self.endowment_min = endowment_min
+        self.endowment_max = endowment_max
+        self.metabolism_min = metabolism_min
+        self.metabolism_max = metabolism_max
+        self.vision_min = vision_min
+        self.vision_max = vision_max
 
         self.grid = mesa.space.MultiGrid(width, height, torus=False)
         self.schedule = mesa.time.RandomActivationByType(self)
+        self.agent_id = 0
+        self.populate_resources()
+        self.generate_traders()
 
+    def populate_resources(self):
         self.sugar_distribution = np.genfromtxt('data/sugar-map.txt')
         self.spice_distribution = np.flip(self.sugar_distribution, axis=1)
 
-        agent_id = 0
-        for _, (x, y) in self.grid.coord_iter():
+        for _, x, y in self.grid.coord_iter():
             for resource, resource_class in zip([self.sugar_distribution, self.spice_distribution], [Sugar, Spice]):
                 max_amount = resource[x, y]
                 if max_amount == 0:
                     continue
-                resource_instance = resource_class(agent_id, self, (x, y), max_amount)
+                resource_instance = resource_class(self.agent_id, self, (x, y), max_amount)
                 self.grid.place_agent(resource_instance, (x, y))
-                print(self.schedule.agents_by_type[resource_class][agent_id])
-                agent_id += 1
+                self.schedule.add(resource_instance)
+                self.agent_id += 1
+    
+    def generate_traders(self):
+        for i in range(self.initial_population):
 
-                # print(_, (x, y))
+            sugar = int(random.uniform(self.endowment_min, self.endowment_max))
+            spice = int(random.uniform(self.endowment_min, self.endowment_max))
+            metab_sugar = int(random.uniform(self.metabolism_min, self.metabolism_max))
+            metab_spice = int(random.uniform(self.metabolism_min, self.metabolism_max))
+            vision = int(random.uniform(self.vision_min, self.vision_max))
+            posn = (np.random.randint(0, self.width), 
+                    np.random.randint(0, self.height))
+            trader = Trader(self.agent_id, 
+                            self,
+                            posn,
+                            sugar = sugar,
+                            spice = spice,
+                            metabolism_sugar = metab_sugar,
+                            metabolism_spice = metab_spice,
+                            vision = vision)
+            self.grid.place_agent(trader, posn) 
+            self.schedule.add(trader)
+            self.agent_id += 1
+            print(trader.unique_id, trader.sugar, trader.metabolism_spice)
 
 
 model = SugarscapeG1mt()
-# plt.imshow(model.sugar_distribution, origin='lower')
-# plt.show()
 
 apple = 1
